9bf786d258cb6604eca90bd7015b3847e5ef2f00
ajlopez <webmaster@ajlopez.com>
2017-12-24 13:06:39 -0300

    Executor set variable

--- test/executor.js
+++ test/executor.js
@@ -46,5 +46,40 @@ exports['execute with variables'] = function (test) {
 	});
 };
 
+exports['execute set variable to constant expression'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.execute('set x 3', function (err, data) {
+		test.ok(!err);
+		test.equal(data, 3);
+		test.equal(executor.value('x'), 3);
+		
+		test.done();
+	});
+};
+
+exports['execute set variable to arithmetic expression using variables'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.value('one', 1);
+	executor.value('two', 2);
+	
+	executor.execute('set x one + two', function (err, data) {
+		test.ok(!err);
+		test.equal(data, 3);
+		test.equal(executor.value('x'), 3);
+		
+		test.done();
+	});
+};
+
+
+
+
+
 
 
