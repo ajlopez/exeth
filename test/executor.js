213ec49648c25c661cca45b1d0547cb1b4348de1
ajlopez <webmaster@ajlopez.com>
2017-12-24 13:26:25 -0300

    Execute script file

--- test/executor.js
+++ test/executor.js
@@ -1,5 +1,6 @@
 
 var exeth = require('..');
+var path = require('path');
 
 exports['get undefined value'] = function (test) {
 	var executor = exeth.executor();
@@ -100,6 +101,34 @@ exports['execute accounts'] = function (test) {
 		test.done();
 	});
 };
+
+exports['execute accounts script'] = function (test) {
+	var provider = createProvider();
+	var accounts = [ '0x01', '0x02', '0x03' ];
+	
+	provider.eth_accounts = function (hash) {
+		return accounts;
+	};
+
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.host(provider);
+	
+	executor.executeFile(getScriptFile('accounts'), function (err, data) {
+		test.ok(!err);
+		test.deepEqual(data, accounts);
+		test.deepEqual(executor.value('accounts'), accounts);
+		test.deepEqual(executor.value('value'), accounts);
+		
+		test.done();
+	});
+};
+
+function getScriptFile(name) {
+	return path.join(__dirname, 'scripts', name + '.eth');
+}
 
 function createProvider() {
 	return {
