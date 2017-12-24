de562e8dadf3c92d9f723a93b8e129c35dc75499
ajlopez <webmaster@ajlopez.com>
2017-12-24 12:54:05 -0300

    Executor evaluate first test

--- test/executor.js
+++ test/executor.js
@@ -11,6 +11,22 @@ exports['set and get value'] = function (test) {
 	var executor = exeth.executor();
 	
 	executor.value('host', 'http://localhost:8545');
+
 	test.equal(executor.value('host'), 'http://localhost:8545');
 };
+
+exports['execute evaluate'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.execute('evaluate 1+2', function (err, data) {
+		test.ok(!err);
+		test.equal(data, 3);
+		test.equal(executor.value('value'), 3);
+		
+		test.done();
+	});
+};
+
 
