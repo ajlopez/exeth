868378c6ad70bacf08133f47049ecdd2e3537da8
ajlopez <webmaster@ajlopez.com>
2017-12-25 12:43:55 -0300

    Execute compile counter.sol

---
+++ test/compile.js
@@ -0,0 +1,25 @@
+
+var exeth = require('..');
+var path = require('path');
+
+exports['compile contract'] = function (test) {
+	test.async();
+	
+	var filename = path.join(__dirname, 'contracts', 'counter.sol');
+	
+	var executor = exeth.executor();
+	
+	executor.execute('compile ' + JSON.stringify(filename), function (err, data) {
+		test.ok(!err);
+		
+		test.ok(executor.value('contracts'));
+		
+		var contracts = executor.value('contracts');
+		
+		test.equal(Object.keys(contracts).length, 2);
+		test.ok(contracts.Counter);
+		test.ok(contracts[filename + ':Counter']);
+		
+		test.done();
+	});
+};
