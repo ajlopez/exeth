ff74d30ecc44a8c0eb7049a9cd77e70866f4cc3e
ajlopez <webmaster@ajlopez.com>
2017-12-25 14:43:16 -0300

    Execute assert

---
+++ test/assert.js
@@ -0,0 +1,28 @@
+
+var exeth = require('..');
+
+exports['execute assert'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.execute('assert 2 > 1', function (err, data) {
+		test.ok(!err);
+		test.equal(data, true);
+
+		test.done();
+	});
+}
+
+exports['execute failed assert'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.execute('assert 2 < 1', function (err, data) {
+		test.ok(err);
+		test.equal(err, "Error: failed assertion 2 < 1");
+
+		test.done();
+	});
+}
