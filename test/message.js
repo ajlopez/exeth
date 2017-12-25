18b8df6cf6204aaeb1d6f46fd6bde1708b729973
ajlopez <webmaster@ajlopez.com>
2017-12-25 09:55:29 -0300

    Execute message with logger

---
+++ test/message.js
@@ -0,0 +1,47 @@
+
+var exeth = require('..');
+
+exports['execute message with an argument'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	var done = false;
+	
+	executor.logger({
+		log: function() {
+			test.equal(arguments.length, 1);
+			test.equal(arguments[0], 42);
+			done = true;
+		}
+	});
+	
+	executor.execute('message 40+2', function (err, data) {
+		test.ok(!err);
+		test.ok(!data);
+		test.ok(done);
+		test.done();
+	});
+}
+
+exports['execute message with two arguments'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	var done = false;
+	
+	executor.logger({
+		log: function() {
+			test.equal(arguments.length, 2);
+			test.equal(arguments[0], "the answer is");
+			test.equal(arguments[1], 42);
+			done = true;
+		}
+	});
+	
+	executor.execute('message "the answer is" 40+2', function (err, data) {
+		test.ok(!err);
+		test.ok(!data);
+		test.ok(done);
+		test.done();
+	});
+}
