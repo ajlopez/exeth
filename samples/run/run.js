a208102ee9764c7b6c34b9c3ca87544a9af0700e
ajlopez <webmaster@ajlopez.com>
2017-12-25 11:37:36 -0300

    Logging using chalk module; logging in run samples

--- samples/run/run.js
+++ samples/run/run.js
@@ -1,7 +1,19 @@
 
 var executor = require('../..').executor();
+var sargs = require('simpleargs');
 
-executor.executeFile(process.argv[2], function (err, data) {
+sargs
+    .define('h', 'host', null, 'Host JSON RPC entry point')
+    .define('f', 'from', null, 'From account address or number')
+	.define('l', 'logging', false, 'Enable logging', { flag: true })
+
+var options = sargs(process.argv.slice(2));
+var args = options._;
+
+if (options.logging)
+	executor.logging(true);
+
+executor.executeFile(args[0], function (err, data) {
 	if (err)
 		console.error(err);
 	else if (data)
