c62dc4e8e9ebfe4875a95e0983bffc59cc9e62b8
ajlopez <webmaster@ajlopez.com>
2017-12-25 09:45:52 -0300

    Execute block

--- test/blocks.js
+++ test/blocks.js
@@ -24,6 +24,30 @@ exports['execute blocknumber'] = function (test) {
 	});
 };
 
+exports['execute block'] = function (test) {
+	var provider = createProvider();
+	
+	provider.eth_getBlockByNumber = function (number) {
+		return {
+			number: number
+		}
+	};
+
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.host(provider);
+	
+	executor.execute('block 42', function (err, data) {
+		test.ok(!err);
+		test.deepEqual(data, { number: '0x2a' });
+		test.deepEqual(executor.value('value'), { number: '0x2a' });
+		
+		test.done();
+	});
+};
+
 function getScriptFile(name) {
 	return path.join(__dirname, 'scripts', name + '.eth');
 }
