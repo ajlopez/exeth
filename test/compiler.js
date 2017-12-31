58398e4db7ceb29a9b06a7bb34dee0858df64225
ajlopez <webmaster@ajlopez.com>
2017-12-31 12:45:47 -0300

    Compiler link bytecodes

--- test/compiler.js
+++ test/compiler.js
@@ -25,3 +25,21 @@ exports['compile contract with library'] = function (test) {
 	
 	test.ok(contracts);
 };
+
+exports['compile contract with unlinked library'] = function (test) {
+	var filename = path.join(__dirname, 'contracts', 'MetaCoin.sol');
+	
+	var contracts = compiler.compileFile(filename);
+	
+	test.ok(contracts);
+	
+	var bytecode = contracts[Object.keys(contracts)[0]].bytecode;
+	
+	test.ok(bytecode.indexOf('_') > 0);
+	test.ok(bytecode.indexOf('ConvertLib') > 0);
+	
+	var linked = compiler.linkBytecode(bytecode, { 'ConvertLib.sol:ConvertLib' : '0x0102030405060708090a0b0c0d0e0f1011121314' });
+	
+	test.ok(linked.indexOf('_') < 0);
+	test.ok(linked.indexOf('ConvertLib') < 0);
+};
