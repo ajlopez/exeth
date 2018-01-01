c80d2c5d87c54f9dbe146a8f3fed8efd1ffa61bd
ajlopez <webmaster@ajlopez.com>
2018-01-01 12:49:33 -0300

    Execute scripts with nested execute commands

--- test/execute.js
+++ test/execute.js
@@ -17,6 +17,24 @@ exports['execute script with two execute commands'] = function (test) {
 	});
 };
 
+exports['execute script with many nested execute commands'] = function (test) {
+	test.async();
+	
+	var executor = exeth.executor();
+	
+	executor.executeFile(getScriptFile('executemany'), function (err, data) {
+		test.ok(!err);
+
+		test.equal(executor.value('first'), true);
+		test.equal(executor.value('second'), true);
+		test.equal(executor.value('third'), true);
+		test.equal(executor.value('fourth'), true);
+		test.equal(executor.value('fifth'), true);
+		
+		test.done();
+	});
+};
+
 function getScriptFile(name) {
 	return path.join(__dirname, 'scripts', name + '.eth');
 }
