edd72781f2ef179759618f47cd4b650c655e50f3
ajlopez <webmaster@ajlopez.com>
2018-01-04 12:11:11 -0300

    Send command

--- test/transaction.js
+++ test/transaction.js
@@ -87,6 +87,71 @@ exports['execute transfer with data'] = function (test) {
 	});
 };
 
+exports['execute send'] = function (test) {
+	test.async();
+	
+	var provider = createProvider();
+	var sent = false;
+	
+	provider.eth_sendTransaction = function (txdata) {
+		test.ok(txdata);
+		test.equal(txdata.from, 100);
+		test.equal(txdata.to, 200);
+		test.equal(txdata.value, 42);
+		test.equal(txdata.gasPrice, 0);
+		test.equal(txdata.gas, 5000000);
+		sent = true;
+		return '0x100';
+	};
+
+	var executor = exeth.executor();
+	
+	executor.host(provider);
+	
+	executor.execute('send 100 200 42', function (err, data) {
+		test.ok(!err);
+		test.equal(data, '0x100');
+		test.deepEqual(executor.value('result'), '0x100');
+		
+		test.ok(sent);
+		
+		test.done();
+	});
+};
+
+exports['execute send with data'] = function (test) {
+	test.async();
+	
+	var provider = createProvider();
+	var sent = false;
+	
+	provider.eth_sendTransaction = function (txdata) {
+		test.ok(txdata);
+		test.equal(txdata.from, 100);
+		test.equal(txdata.to, 200);
+		test.equal(txdata.value, 42);
+		test.equal(txdata.gasPrice, 0);
+		test.equal(txdata.gas, 5000000);
+		test.equal(txdata.data, "0x60606040");
+		sent = true;
+		return '0x100';
+	};
+
+	var executor = exeth.executor();
+	
+	executor.host(provider);
+	
+	executor.execute('send 100 200 42 "60606040"', function (err, data) {
+		test.ok(!err);
+		test.equal(data, '0x100');
+		test.deepEqual(executor.value('result'), '0x100');
+		
+		test.ok(sent);
+		
+		test.done();
+	});
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
