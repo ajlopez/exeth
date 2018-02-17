7f4c89259646c1373dc3598a35c34f29082683cb
ajlopez <webmaster@ajlopez.com>
2018-02-17 11:25:55 -0300

    Using cli.js in samples run

--- samples/run/run.js
+++ samples/run/run.js
@@ -1,25 +1,3 @@
 
-var executor = require('../..').executor();
-var sargs = require('simpleargs');
-
-sargs
-    .define('h', 'host', null, 'Host JSON RPC entry point')
-    .define('f', 'from', null, 'From account address or number')
-	.define('l', 'logging', false, 'Enable logging', { flag: true })
-
-var options = sargs(process.argv.slice(2));
-var args = options._;
-
-if (options.logging)
-	executor.logging(true);
-
-if (options.host)
-	executor.host(options.host);
-
-executor.executeFile(args[0], function (err, data) {
-	if (err)
-		console.error(err);
-	else if (data)
-		console.dir(data);
-});
+require('../../cli.js');
 
