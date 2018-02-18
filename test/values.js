811a0dd5dee312d520ce8a8b5b49ddcac68e09f0
ajlopez <webmaster@ajlopez.com>
2018-02-18 17:00:21 -0300

    Async evaluation

--- test/values.js
+++ test/values.js
@@ -28,3 +28,19 @@ exports['evaluate expression with variables'] = function (test) {
 	
 	test.equal(vals.evaluate('one + two'), 3);
 }
+
+exports['evaluate expression with variables and callback'] = function (test) {
+	var vals = values.values();
+	
+	test.async();
+	
+	vals.value('one', 1);
+	vals.value('two', 2)
+	
+	vals.asyncEvaluate('cb(null, one + two)', function (err, data) {
+		test.ok(!err);
+		test.equal(data, 3);
+		
+		test.done();
+	});
+}
